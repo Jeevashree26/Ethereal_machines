1649d582462092a0e01590c4a1b89beaf7bf7d3d
Jeevashreeplotch <142967135+Jeevashreeplotch@users.noreply.github.com>
2024-09-23 19:02:07 +0530

    changes

--- ethereal_machines/machines/models.py
+++ ethereal_machines/machines/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
+
 
 class Machine(models.Model):
     machine_name = models.CharField(max_length=255, unique=True)
     tool_capacity = models.PositiveIntegerField(validators=[MinValueValidator(1)])  # Ensures tool capacity is positive
     tool_offset = models.FloatField(validators=[MinValueValidator(0.0)])  # Ensures no negative offsets
     feedrate = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10000)])  # Feedrate has a reasonable max limit
-    tool_in_use = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(255)])  # Range limit for tool_in_use, depends on use case
+    tool_in_use = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(255)])  # Range limit for tool_in_use
 
     created_at = models.DateTimeField(auto_now_add=True)  # Tracks when machine was created
     updated_at = models.DateTimeField(auto_now=True)  # Tracks last update time
@@ -17,8 +19,15 @@ class Machine(models.Model):
 
 
 class Axis(models.Model):
+    class AxisType(models.TextChoices):
+        X = 'X', _('X Axis')
+        Y = 'Y', _('Y Axis')
+        Z = 'Z', _('Z Axis')
+        A = 'A', _('A Axis')
+        C = 'C', _('C Axis')
+
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE, related_name='axes')
-    axis_name = models.CharField(max_length=5)  # Short name but could be more descriptive
+    axis_name = models.CharField(max_length=1, choices=AxisType.choices)  # Only allow predefined axis types
     max_acceleration = models.FloatField(validators=[MinValueValidator(0.0)])  # Acceleration must be non-negative
     max_velocity = models.FloatField(validators=[MinValueValidator(0.0)])  # Velocity must be non-negative
 
@@ -26,7 +35,7 @@ class Axis(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.axis_name} - {self.machine.machine_name}"
+        return f"{self.get_axis_name_display()} - {self.machine.machine_name}"  # Get human-readable axis name
 
 
 class FieldUpdate(models.Model):
